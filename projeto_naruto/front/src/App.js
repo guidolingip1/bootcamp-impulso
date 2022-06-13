7a61dfef21f0563d667044949f99d64a827b741a
guidolingip <guidolingip1@gmail.com>
2022-06-13 13:05:39 -0300

    Fazendo o sound funcionar e printando a minha API

--- projeto_naruto/front/src/App.js
+++ projeto_naruto/front/src/App.js
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import narutoImg from "./images/naruto.png";
 import { Quotes } from "./components/Quotes";
+import jutsoSound from "./sounds/jutso.mp3";
 import { getQuote } from "./services";
 import { useState } from "react";
 
+const audio = new Audio(jutsoSound);
+
 function App() {
-  const [quoteState, setQuoteState] = useState({ quote: "OK", speaker: "Speaker" });
+  const [quote, setQuote] = useState({ speaker: "Speaker", quote: "OK" });
 
   const onUpdate = async () => {
-    const quote = await getQuote();
-
-    setQuoteState(quote);
+    const newQuote = await getQuote();
+    setQuote({ quote: newQuote.mensagem });
+    audio.play();
   };
 
   return (
     <Content>
-      <Quotes quote={quoteState.quote} speaker={quoteState.speaker} onUpdate={onUpdate} />
+      <Quotes {...quote} onUpdate={onUpdate} />
       <NarutoImg src={narutoImg} alt="Naruto with a kunai" />
     </Content>
   );
