857f95abe0968536a9cc29791f2c2ef794d91f86
guidolingip <guidolingip1@gmail.com>
2022-06-13 13:06:50 -0300

    Arrumando um erro de versão

--- projeto_naruto/front/src/index.js
+++ projeto_naruto/front/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "./components/GlobalStyle/GlobalStyle";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
