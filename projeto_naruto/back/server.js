7e8c576d6e278b0a6816c115ca73adbfc3fd83f2
guidolingip <guidolingip1@gmail.com>
2022-06-13 12:37:42 -0300

    Deletando arquivos da raiz e passando eles para Front/back

--- projeto_naruto/back/server.js
+++ projeto_naruto/back/server.js
@@ -1,13 +1,32 @@
 const cors = require("cors");
 const express = require("express");
-const app = express();
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
 const port = 5000;
-
+const app = express();
 app.use(cors());
 
+const Quotes = [];
+
+// Lê o CSV pega somente as frases e adiona elas no array Quotes
+fs.createReadStream("finalQuotes.csv")
+  .pipe(
+    parse({
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    Quotes.push(data[1]);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
-  res.send({ mensagem: "Hello World!" });
+  // sorteia uma frase do array Quotes
+  let quote = Quotes[Math.floor(Math.random() * Quotes.length)];
+  res.send({ mensagem: quote });
 });
 
 app.listen(port, () => {
