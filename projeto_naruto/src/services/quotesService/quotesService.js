269dfbee5d72d0fd22a3b13e01fb5d1c74ac67ed
guidolingip <guidolingip1@gmail.com>
2022-06-12 23:06:50 -0300

    Finalizando o projeto

--- projeto_naruto/src/services/quotesService/quotesService.js
+++ projeto_naruto/src/services/quotesService/quotesService.js
@@ -1,3 +1,10 @@
-export const quotesService = () => {
-  fetch("http://127.0.0.1:3000").then((response) => response.json());
+export const getQuote = async () => {
+  const req = await fetch(`http://allugofrases.herokuapp.com/frases/random`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = await req.json();
+  return json.frase;
 };
