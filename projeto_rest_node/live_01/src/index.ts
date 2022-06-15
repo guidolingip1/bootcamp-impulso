71b55ede4602d2a7ee8975539d7d8b1b073464c2
guidolingip <guidolingip1@gmail.com>
2022-06-15 14:16:32 -0300

    Adicionando um server reloader

--- projeto_rest_node/live_01/src/index.ts
+++ projeto_rest_node/live_01/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Executei");
+import express, { Request, Response, NextFunction } from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/status", (request: Request, response: Response, next: NextFunction) => {
+  response.status(200).send({ foo: "Sucesso" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running in http://localhost:${PORT}`);
+});
